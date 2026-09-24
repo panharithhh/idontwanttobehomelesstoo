c72beccbc16a13c325092e6638fe23f44549b7cc
panharithhh <161872594+panharithhh@users.noreply.github.com>
2026-09-24 13:05:42 +0700

    wip: autosave 2026-09-24 13:05 (2 file(s))

---
+++ cs336_basics/tokenizer.py
@@ -0,0 +1,36 @@
+def get_stats() -> list:
+    stats = {}  
+
+
+
+
+    return stats 
+
+def merge():
+
+    pass
+
+
+class minbpe():
+
+    def __init__(self, text_file, ):
+        pass
+
+    def train():
+        pass
+    
+    def encode():
+        pass
+
+    def decode():
+        pass
+    
+    
+    with open("data/TinyStoriesV2-GPT4-valid.txt", mode = "r") as  f:
+
+        data = f.read("utf-8")
+
+
+
+
+
